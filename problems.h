c9aeb8fe7059c2a6086ea14e4c4e66bb25b60713
scapemm <a.shukurullayev@newuu.uz>
2024-11-22 11:29:31 +0500

    Create problems.h

---
+++ problems.h
@@ -0,0 +1,27 @@
+
+
+void days2years(int &days, int &years) {
+    years = days/365;
+    days %= 365;
+}
+// Problem 2
+double func(double *x,double *y) {
+    
+}
+
+
+// Problem 3
+int minsNewYear(int *hour, int *min) {
+    
+}
+
+// Problem 4
+double probability(int *y,int *w) {
+   
+}
+
+// Problem 5
+int presses(int *x) {
+    
+}
+
